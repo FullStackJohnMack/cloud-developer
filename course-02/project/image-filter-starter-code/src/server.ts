04873f62319c0f653497684283dc6e9c2e33a5fa
John Mack <mackspace@gmail.com>
2020-06-06 19:19:02 -0600

    fixed 502 errors, working

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -9,7 +9,7 @@ import { resolveSoa } from 'dns';
   const app = express();
 
   // Set the network port 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
